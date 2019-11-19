51fdc50b5cd6ed2e39d66ea7cf859555beefc772
J. David Giese <johndgiese@gmail.com>
2019-11-19 10:48:02 -0500

    Add second tutorial message

--- recieve.py
+++ recieve.py
@@ -1,16 +1,23 @@
 #!/usr/bin/env python
+import time
+
 import pika
 
 connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
 channel = connection.channel()
-channel.queue_declare(queue='hello')
+channel.queue_declare(queue='task_queue', durable=True)
 
 def callback(ch, method, properties, body):
-    print(" [x] Received %r" % body)
+    try:
+        print(f" [x] Received {body}")
+        time.sleep(body.count(b'.'))
+        print(f" [x] Done")
+        ch.basic_ack(delivery_tag = method.delivery_tag)
+    except:
+        print('error')
 
-channel.basic_consume(queue='hello',
-                      auto_ack=True,
-                      on_message_callback=callback)
+channel.basic_qos(prefetch_count=1)
+channel.basic_consume(queue='task_queue', on_message_callback=callback)
 
 print(' [*] Waiting for messages. To exit press CTRL+C')
 channel.start_consuming()
